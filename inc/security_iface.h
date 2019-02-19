8d18cda10c20277211cfc27781b201b7bdb2d2e9
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2019-02-19 20:44:29 +0530

    add reference methods to security iface

--- inc/security_iface.h
+++ inc/security_iface.h
@@ -13,4 +13,85 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
- 
+
+#include <stdint.h>
+
+
+// User Implemented Methods
+
+/**
+ * @brief   Sets up Security module and return handler.
+ * @details Implementer of this method needs initialise security 
+ *          mechanism as specified by the regulation and share the 
+ *          handle to the same via this method
+ *
+ * @param[in] npnt_handle      npnt handle
+ * 
+ * @return              pointer to security handler.
+ * @retval NULL         security handler failed to initialise.
+ *
+ * @iclass security_iface
+ */
+void* npnt_set_security_handle(npnt_s *npnt_handle);
+
+
+/**
+ * @brief   Checks if the raw data is authentic.
+ * @details Implementer of this method needs to check the authenticity
+ *          of raw data with signature against the public key provided 
+ *          by DGCA Server.
+ *
+ * @param[in] npnt_handle        npnt handle
+ * @param[in] raw_data           signed raw data to be authenticated
+ * @param[in] raw_data_len       signed raw data to be authenticated
+ * @param[in] signature          signature of signed raw data
+ * @param[in] signature_len      length of signature
+ * 
+ * @return           Errcode of authentication check, 0 if authentication was successful
+ * @retval 0         Successful Authentication
+ *
+ * @iclass security_iface
+ */
+int npnt_check_authenticity(npnt_s *npnt_handle, uint8_t* raw_data, uint16_t raw_data_len, uint8_t* signature, uint16_t signature_len);
+
+/**
+ * @brief   Signs raw data.
+ * @details Implementer of this method needs to sign the raw data
+ *          and signature against the private key generated in-system.
+ *
+ * @param[in] npnt_handle        npnt handle
+ * @param[in] raw_data           signed raw data to be authenticated
+ * @param[in] raw_data_len       signed raw data to be authenticated
+ * @param[in] signature          signature of signed raw data
+ * @param[in] signature_len      length of signature
+ * @param[out] signature_len     updated length of signature
+ * 
+ * @return           Errcode of signature failure, 0 if signature was generated successfully
+ * @retval 0         Successfully Signed
+ *
+ * @iclass security_iface
+ */
+int npnt_sign_raw_data(npnt_s *npnt_handle, uint8_t* raw_data, uint16_t raw_data_len, uint8_t* signature, uint16_t* signature_len);
+
+
+//Implemented by libnpnt
+
+/**
+ * @brief   Initialise Security interface.
+ * @details This method needs to sign the raw data
+ *          and signature against the private key generated in-system.
+ *
+ * @param[in] npnt_handle        npnt handle
+ * @param[in] raw_data           signed raw data to be authenticated
+ * @param[in] raw_data_len       signed raw data to be authenticated
+ * @param[in] signature          signature of signed raw data
+ * @param[in] signature_len      length of signature
+ * @param[out] signature_len     updated length of signature
+ * 
+ * @return           Errcode of signature failure, 0 if signature was generated successfully
+ * @retval 0         Successfully Signed
+ *
+ * @iclass security_iface
+ */
+
+int npnt_security_init(npnt_s* npnt_handle);
