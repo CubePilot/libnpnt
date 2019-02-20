aeba52e38e17c8f4c1274aa55d9b1816f2c50cfd
Siddharth Purohit <siddharthbharatpurohit@gmail.com>
2019-02-20 17:08:28 +0530

    modify license header to MPL

--- test/test_ifaces.cpp
+++ test/test_ifaces.cpp
@@ -1,19 +1,9 @@
 /*
- * This file is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This file is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/. 
  */
- 
+
  /**
  * @file    test/test_ifaces.h
  * @brief   Test libnpnt interfaces
@@ -23,3 +13,6 @@
  #include <control_iface.h>
  #include <log_iface.h>
  #include <security_iface.h>
+
+ /** @} */
+ 
